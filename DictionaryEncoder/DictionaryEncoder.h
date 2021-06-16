acc05313f12f9ec2534a21b80781bba633158235
Aris Koxaras <aris.koxaras@vodafone.com>
2021-06-16 20:32:54 +0300

    Initial Commit

---
+++ DictionaryEncoder/DictionaryEncoder.h
@@ -0,0 +1,18 @@
+//
+//  DictionaryEncoder.h
+//  DictionaryEncoder
+//
+//  Created by Koxaras, Aris, Vodafone Greece on 16/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DictionaryEncoder.
+FOUNDATION_EXPORT double DictionaryEncoderVersionNumber;
+
+//! Project version string for DictionaryEncoder.
+FOUNDATION_EXPORT const unsigned char DictionaryEncoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DictionaryEncoder/PublicHeader.h>
+
+
